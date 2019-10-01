23ec0cc42dd2e95a9f0f2ac4526bc2853d87888a
Jorge Cruz <jorge@dxt-002015.home>
2019-09-30 22:46:41 -0300

    Adicionando os arquivos base do projeto.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# aws-ecs-cookbook
-Alguns experimentos com o serviço ECS da Amazon e Terraform. Se tiver interesse de aprender junto comigo é só chamar!
+# ECS Cookbook
+*Alguns experimentos com o serviço ECS da Amazon e Terraform.*
+
+Antes de executar este código, crie um arquivo de extensão **.tfvars** com o seguinte conteúdo:
+
+```
+access_key = "seu-access-key-vai-aqui"
+secret_key = "seu-secret-key-vai-aqui"
+```
+
+Para este exemplo, criei o arquivo **production.tfvars**, e para verificar se o plano de execução está correto, utilize o comando a seguir:
+
+```
+terraform plan -var-file=production.tfvars
+```
+
+Se tudo deu certo (espero que sim) execute o comando a seguir para provisionar a infra:
+
+```
+terraform apply -var-file=production.tfvars
+```
+
+Para desprovisionar:
+
+```
+terraform destroy -var-file=production.tfvars
+```
+
+Se tiver interesse de aprender junto comigo é só chamar, e se viu algum erro abra uma issue ...
---
+++ variables.tf
@@ -0,0 +1,56 @@
+variable "aws_region" {
+  description = "The AWS region things are created in"
+  default     = "us-east-1"
+}
+
+variable "access_key" {}
+variable "secret_key" {}
+
+variable "ecs_task_execution_role_name" {
+  description = "ECS task execution role name"
+  default = "myEcsTaskExecutionRole"
+}
+
+variable "ecs_auto_scale_role_name" {
+  description = "ECS auto scale role Name"
+  default = "myEcsAutoScaleRole"
+}
+
+variable "az_count" {
+  description = "Number of AZs to cover in a given region"
+  default     = "2"
+}
+
+variable "app_image" {
+  description = "Docker image to run in the ECS cluster"
+  default     = "nginx:stable-alpine"
+}
+
+variable "app_port" {
+  description = "Port exposed by the docker image to redirect traffic to"
+  default     = 80
+}
+
+variable "app_count" {
+  description = "Number of docker containers to run"
+  default     = 3
+}
+
+variable "health_check_path" {
+  default = "/"
+}
+
+variable "fargate_cpu" {
+  description = "Fargate instance CPU units to provision"
+  default     = "256"
+}
+
+variable "fargate_memory" {
+  description = "Fargate instance memory to provision (in MiB)"
+  default     = "512"
+}
+
+variable "ecs_username" {
+  description = "User to manage ECS instances"
+  default = "ecs_user"
+}
